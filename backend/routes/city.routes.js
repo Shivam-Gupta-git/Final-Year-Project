a8f0d40efe1efe5d9f064c852787803a22ecfb00
ramansingh19 <raman.01kumar1@gmail.com>
2026-02-12 22:29:01 +0530

    get active cities

--- backend/routes/city.routes.js
+++ backend/routes/city.routes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { createCity } from "../controllers/city.controller.js"
+import { createCity, getActiveCities } from "../controllers/city.controller.js"
 import { upload } from "../middleware/multer.middleware.js"
 
 const router = express.Router()
 
 router.post("/", upload.array("images" , 5) ,  createCity) 
+router.get("/getactive-city", getActiveCities)
 
 export default router
