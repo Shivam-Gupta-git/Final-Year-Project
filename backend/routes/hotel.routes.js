edd216b027abfa0157faa4dc48741cc56b0df045
ramansingh19 <raman.01kumar1@gmail.com>
2026-02-18 17:22:03 +0530

    hotel controller

--- backend/routes/hotel.routes.js
+++ backend/routes/hotel.routes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { createHotel } from "../controllers/hotel.controller.js";
+import { createHotel, getHotelbyid } from "../controllers/hotel.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 
 const hotelRouter = express.Router();
 
 hotelRouter.post("/" , upload.array("images" , 5), createHotel)
+hotelRouter.get("/", getHotelbyid)
 
 export default hotelRouter;
