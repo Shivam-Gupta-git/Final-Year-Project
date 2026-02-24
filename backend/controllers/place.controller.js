f28b7e74290236af8ca3bccc046a9512e93f950d
ramansingh19 <raman.01kumar1@gmail.com>
2026-02-24 12:14:45 +0530

    Add getActivePlace endpoint and improve createPlace error handling

--- backend/controllers/place.controller.js
+++ backend/controllers/place.controller.js
@@ -1,7 +1,7 @@
+import fs from "fs";
 import { uploadCloudinary } from "../config/cloudinary.config.js";
 import { City } from "../model/city.model.js";
 import { Place } from "../model/place.model.js";
-import fs from "fs"
 
 export const createPlace = async (req, res) => {
   try {
@@ -23,15 +23,15 @@ export const createPlace = async (req, res) => {
 
     let location;
     try {
-      location = JSON.parse(req.body.location)
+      location = JSON.parse(req.body.location);
     } catch (error) {
       return res.status(400).json({
-        success : false,
-        message : "Inavalid location format"
-      })
+        success: false,
+        message: "Inavalid location format",
+      });
     }
     console.log(location);
-    
+
     if (
       !name ||
       !cityId ||
@@ -43,59 +43,58 @@ export const createPlace = async (req, res) => {
       !bestTimeToVisit
     ) {
       return res.status(400).json({
-        success : false,
-        message : "All fields are required"
-      })
+        success: false,
+        message: "All fields are required",
+      });
     }
 
     //verify city
-    const city = await City.findOne({ _id : cityId , status : "active"})
+    const city = await City.findOne({ _id: cityId, status: "active" });
     if (!city) {
       return res.status(400).json({
-        success : false,
-        message : "wrong city id"
-      })
+        success: false,
+        message: "wrong city id",
+      });
     }
     console.log(city);
-    
 
     //verify place by city
     const existingPlace = await Place.findOne({
       name,
-      cityId
-    })
+      cityId,
+    });
     if (existingPlace) {
       return res.status(409).json({
-        success : false,
-        message : "Place is already exits in this city "
-      })
+        success: false,
+        message: "Place is already exits in this city ",
+      });
     }
     console.log(existingPlace);
-    
+
     //privent location duplicate
     const exitinglocation = await Place.findOne({
-      "location.coordinates" : location.coordinates
-    })
+      "location.coordinates": location.coordinates,
+    });
     if (exitinglocation) {
       return res.status(409).json({
         success: false,
-        message: "A place already exists at this location"
+        message: "A place already exists at this location",
       });
     }
 
     let imageUrl = [];
     if (req.files?.length) {
-      for(const file of req.files){
-        const result = await uploadCloudinary(file.path, "places")
+      for (const file of req.files) {
+        const result = await uploadCloudinary(file.path, "places");
         imageUrl.push(result.secure_url);
-        //unlink when upload to cloudnary 
+        //unlink when upload to cloudnary
         try {
-      if (fs.existsSync(file.path)) {
-        fs.unlinkSync(file.path);
-      }
-    } catch (err) {
-      console.warn("File delete skipped:", err.message);
-    }
+          if (fs.existsSync(file.path)) {
+            fs.unlinkSync(file.path);
+          }
+        } catch (err) {
+          console.warn("File delete skipped:", err.message);
+        }
       }
     }
 
@@ -108,19 +107,18 @@ export const createPlace = async (req, res) => {
       entryfees,
       category,
       timeRequired,
-      images : imageUrl,
+      images: imageUrl,
       location,
-      status : "pending",
-      createdBy : req.user?._id
-    })
+      status: "pending",
+      createdBy: req.user?._id,
+    });
     console.log(place);
-    
-    return res.status(201).json({
-      success : true,
-      data : place,
-      success : "place created successfully"
-    })
 
+    return res.status(201).json({
+      success: true,
+      data: place,
+      success: "place created successfully",
+    });
   } catch (error) {
     return res.status(500).json({
       success: false,
@@ -131,31 +129,30 @@ export const createPlace = async (req, res) => {
 
 export const approvePlace = async (req, res) => {
   try {
-    const place = await Place.findById(req.params.id)
+    const place = await Place.findById(req.params.id);
     console.log(place);
     if (!place) {
       return res.status(400).json({
-        success : false,
-        message : "not found place "
-      })
+        success: false,
+        message: "not found place ",
+      });
     }
 
     place.status = "active";
-    place.approvedBy = req.user?._id //super admin
-    await place.save()
+    place.approvedBy = req.user?._id; //super admin
+    await place.save();
 
     return res.status(200).json({
-      success : true,
-      message : "place approved"
-    })
-    
+      success: true,
+      message: "place approved",
+    });
   } catch (error) {
     return res.status(500).json({
-      success : false,
-      message : error.message
-    })
+      success: false,
+      message: error.message,
+    });
   }
-}
+};
 
 export const rejectPlace = async (req, res) => {
   try {
@@ -178,19 +175,51 @@ export const rejectPlace = async (req, res) => {
   }
 };
 
-export const pendingPlace = async(req, res) => {
+export const pendingPlace = async (req, res) => {
   try {
-    const place = await Place.find({status : "pending"}).populate("createdBy", "userName email role")
+    const place = await Place.find({ status: "pending" }).populate(
+      "createdBy",
+      "userName email role",
+    );
 
     return res.status(200).json({
-      success : true,
-      data : place,
-      count : place.length
-    })
+      success: true,
+      data: place,
+      count: place.length,
+    });
   } catch (error) {
     return res.status(500).json({
-      success : false,
-      message : error.message
-    })
+      success: false,
+      message: error.message,
+    });
   }
-}
+};
+
+export const getActivePlace = async (req, res) => {
+  try {
+    const { placeId } = req.query;
+
+    if (!placeId) {
+      return res.status(400).json({
+        success: false,
+        message: "placId not found",
+      });
+    }
+
+    const place = await Place.find({
+      placeId,
+      status: "active",
+    }).populate("city", "name state");
+
+    return res.status(200).json({
+      success: true,
+      data: place,
+      message: "get Active place successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: true,
+      message: error.message,
+    });
+  }
+};
--- backend/routes/place.routes.js
+++ backend/routes/place.routes.js
@@ -1,10 +1,13 @@
 import express from "express"
 import { isAuthenticated, authorize } from "../middleware/auth.middleware.js";
-import { createPlace } from "../controllers/place.controller.js";
+import { createPlace, getActivePlace } from "../controllers/place.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 
 const placeRouter = express.Router();
 
 placeRouter.post("/", isAuthenticated, authorize("admin"), upload.array("images" , 5), createPlace)
+
+//public
+placeRouter.get("/getactive" ,  getActivePlace)
 
 export default placeRouter;
