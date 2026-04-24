3bf8dd358f95f1ba54789cb30b396a357aaa1d46
Raman Kumar <raman.01kumar1@gmail.com>
2026-04-24 18:56:08 +0530

    Update API client base URL to production
    fvf

--- frontend/src/pages/services/apiClient.js
+++ frontend/src/pages/services/apiClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const apiClient = axios.create({
   baseURL: 
-  "http://localhost:3000" || "https://travel-b.onrender.com",
+  "https://travel-b.onrender.com",
    // backend URL
   // http://localhost:3000" ||
   withCredentials: true,
